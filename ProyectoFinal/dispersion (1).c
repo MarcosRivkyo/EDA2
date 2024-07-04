245c9d02e326d375938a82d2efe1eb894cdfae9e
Marcos Rivas <114361836+MarcosRivkyo@users.noreply.github.com>
2024-07-04 15:30:44 +0200

    Add files via upload

---
+++ ProyectoFinal/dispersion (1).c
@@ -0,0 +1,52 @@
+#include "dispersion.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Lee el contenido del fichero hash organizado mediante el método de DISPERSIÓN según los criterios
+// especificados en la práctica. Se leen todos los cubos completos tengan registros asignados o no. La
+// salida que produce esta función permite visualizar el método de DISPERSIÓN
+
+int leeHash(char *fichHash)
+{ FILE *f;
+  tipoCubo cubo;
+  regConfig regC;
+  int j,nCubo=0,densidadOcupacion;
+
+   f = fopen(fichHash,"rb");
+   fread(&regC,sizeof(regConfig),1,f);
+   fread(&cubo,sizeof(cubo),1,f); 
+   while (!feof(f)){
+	for (j=0;j<C;j++) {
+        	if (j==0) printf("Cubo %2d (%2d reg. ASIGNADOS)",nCubo,cubo.numRegAsignados);
+        	else 	if ((j==1) && cubo.desbordado) printf("DESBORDADO\t\t");
+        		else printf("\t\t\t");
+		if (j < cubo.numRegAsignados) 
+		    printf("\t%s %s %s %s %s\n",cubo.reg[j].dni, cubo.reg[j].nombre,cubo.reg[j].ape1,cubo.reg[j].ape2,
+			    		cubo.reg[j].provincia);
+	    	else printf ("\n");
+        	}
+		nCubo++;
+       fread(&cubo,sizeof(cubo),1,f);
+   }
+   fclose(f);
+   printf("ORGANIZACIÓN MÉTODO DISPERSIÓN:\n\t %d CUBOS y %d CUBOS DESBORDE con capacidad %d\n",
+   		regC.nCubos,regC.nCubosDes, C);   
+   printf("\t Contiene %d registros de los cuales se han desbordado %d:\n",regC.numReg,regC.numRegDes);
+   densidadOcupacion = 100*regC.numReg/((regC.nCubos+regC.nCubosDes)*C);
+   printf("Densidad ACTUAL de ocupación: %d \n\t(MÍNIMA permitida %d  MÁXIMA permitida %d)\n",
+   				densidadOcupacion,regC.densidadMin,regC.densidadMax);
+   if (densidadOcupacion>regC.densidadMax) {
+   	printf("No se respeta la densidad máxima de ocupacion\n");
+   	return -3;
+   }	
+   if (densidadOcupacion<regC.densidadMin) {
+   	printf("No se respeta la densidad mínima de ocupacion\n");
+   	return -4;
+   }	
+   if ((densidadOcupacion > regC.densidadMin) && (densidadOcupacion < regC.densidadMax))
+      	return 0;
+   
+   return -1;	
+}
+
